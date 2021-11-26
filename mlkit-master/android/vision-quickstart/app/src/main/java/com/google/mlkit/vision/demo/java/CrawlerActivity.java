d27d1d7981d8cb6d1c5d4a91fa87da2ca2756f85
mpafit02 <mariospafitis1998@gmail.com>
2021-11-26 03:11:47 +0200

    Changed MainActivity + Created CrawlerActivity

---
+++ mlkit-master/android/vision-quickstart/app/src/main/java/com/google/mlkit/vision/demo/java/CrawlerActivity.java
@@ -0,0 +1,15 @@
+package com.google.mlkit.vision.demo.java;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.mlkit.vision.demo.R;
+
+import android.os.Bundle;
+
+public class CrawlerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crawler);
+    }
+}
